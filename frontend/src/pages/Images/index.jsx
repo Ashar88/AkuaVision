7c7bb95a96831e74f694646548b9d23f64ac2d8b
Ashar <asher.saleem@puresquare.com>
2024-05-11 20:57:05 +0500

    CMT-10 Added Upload files scrollable and preview is done

--- frontend/src/pages/Images/index.jsx
+++ frontend/src/pages/Images/index.jsx
@@ -1,60 +1,88 @@
 import React, { useState } from 'react';
-import { InboxOutlined } from '@ant-design/icons';
-import { message, Upload } from 'antd';
+import { InboxOutlined, PlusOutlined } from '@ant-design/icons';
+import { message, Upload, Image } from 'antd';
 import "./app.css";
-
 const { Dragger } = Upload;
+
+const getBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+
 const App = () => {
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+  const [fileList, setFileList] = useState();
 
-  const [fileList, setFileList] = useState(false);
-  const props = {
-    name: 'file',
-    multiple: true,
-    action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
-    beforeUpload: (file) => {
-        const isImage = file.type.startsWith('image/');
-        if (!isImage) {
-          message.error(`${file.name} is not an image file.`);
-          return false;
-        }
-        return isImage;
-    },
-    onChange(info) {
-      const { status } = info.file;
-      if (status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-      if (status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully.`);
-      } else if (status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-      if(info.fileList.length>0) {setFileList(true); console.log("setfile-true", info.fileList, fileList)}
-      else if(info.fileList.length===0) {; setFileList(false); console.log("setfile-false", info.fileList, fileList)}
-
-      console.log(fileList,info.fileList.length ,info.fileList)
-    },
-    onDrop(e) {
-      console.log('Dropped files', e.dataTransfer.files);
-    },
-    listType: "picture",
-    
+const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
   };
 
-return (
-  <div className="App">
+
+  const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
+
+  return (
+    <div className="App">
       <h2 className="header">File Upload</h2>
-      <Dragger {...props} className="parent">
+      <Dragger
+        name="file"
+        multiple
+        action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+        beforeUpload={(file) => {
+          const isImage = file.type.startsWith('image/');
+          if (!isImage) {
+            message.error(`${file.name} is not an image file.`);
+            return false;
+          }
+          return isImage;
+        }}
+        onChange={(info) => {
+          const { status } = info.file;
+          if (status !== 'uploading') {
+            console.log(info.file, info.fileList);
+          }
+          if (status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully.`);
+          } else if (status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+          }
+          handleChange(info);
+        }}
+        listType= "picture"
+        fileList={fileList}
+        onPreview={handlePreview}
+        className="parent"
+      >
         <p className="ant-upload-drag-icon">
           <InboxOutlined />
         </p>
         <p className="ant-upload-text">Click or drag file to this area to upload</p>
         <p className="ant-upload-hint">
-          Support for a single or bulk upload. Strictly prohibited from uploading company data or other
-          banned files.
+          Support for a single or bulk upload. Strictly prohibited from uploading company data or other banned files.
         </p>
       </Dragger>
-  </div>)
-}
+        {previewImage && (
+        <Image
+          wrapperStyle={{
+            display: 'none',
+          }}
+          preview={{
+            visible: previewOpen,
+            onVisibleChange: (visible) => setPreviewOpen(visible),
+            afterOpenChange: (visible) => !visible && setPreviewImage(''),
+          }}
+          src={previewImage}
+        />
+      )}
+    </div>
+  );
+};
 
 export default App;
