983f4a26e99b805d25b5cb6d289178317457b18d
Ashar <asher.saleem@puresquare.com>
2024-05-11 20:34:35 +0500

    CMT-9 Added the rough image upload feature

--- frontend/src/pages/Images/index.jsx
+++ frontend/src/pages/Images/index.jsx
@@ -1,42 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InboxOutlined } from '@ant-design/icons';
 import { message, Upload } from 'antd';
+import "./app.css";
 
 const { Dragger } = Upload;
+const App = () => {
 
-const props = {
-  name: 'file',
-  multiple: true,
-  action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log('Dropped files', e.dataTransfer.files);
-  },
-};
+  const [fileList, setFileList] = useState(false);
+  const props = {
+    name: 'file',
+    multiple: true,
+    action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
+    beforeUpload: (file) => {
+        const isImage = file.type.startsWith('image/');
+        if (!isImage) {
+          message.error(`${file.name} is not an image file.`);
+          return false;
+        }
+        return isImage;
+    },
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+      if(info.fileList.length>0) {setFileList(true); console.log("setfile-true", info.fileList, fileList)}
+      else if(info.fileList.length===0) {; setFileList(false); console.log("setfile-false", info.fileList, fileList)}
 
-const App = () => (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
-    <Dragger {...props} style={{ width: '900px', padding: '20px', textAlign: 'center' }}>
-      <p className="ant-upload-drag-icon">
-        <InboxOutlined />
-      </p>
-      <p className="ant-upload-text">Click or drag file to this area to upload</p>
-      <p className="ant-upload-hint">
-        Support for a single or bulk upload. Strictly prohibited from uploading company data or other
-        banned files.
-      </p>
-    </Dragger>
-  </div>
-);
+      console.log(fileList,info.fileList.length ,info.fileList)
+    },
+    onDrop(e) {
+      console.log('Dropped files', e.dataTransfer.files);
+    },
+    listType: "picture",
+    
+  };
+
+return (
+  <div className="App">
+      <h2 className="header">File Upload</h2>
+      <Dragger {...props} className="parent">
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-hint">
+          Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+          banned files.
+        </p>
+      </Dragger>
+  </div>)
+}
 
 export default App;
